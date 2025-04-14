151851917bec48db753260e941f6f59185c9e9b2
Nicholas <mercy.recommends@gmail.com>
2025-04-14 21:55:43 +0100

    Binding mode and wrapper script to run Conky

---
+++ .config/conky/scripts/mode_conky.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+ACTION=$1
+
+# Define the conky config dir
+CONFIG_DIR="$HOME/.config/conky"
+
+# Get all the .conf files
+#CONFIGS=($(find "$CONKY_CONFIG_DIR" -maxdepth 1 -name "*.conf" | sort))
+CONFIGS=("$CONFIG_DIR"/*.conf)
+
+# Check if conky is running
+IS_ACTIVE=$(pgrep -af "conky -c $CONFIG_DIR" | head -n1)
+
+if [[ -n "$IS_ACTIVE" ]]; then
+    ACTIVE_CONF=$(echo "$IS_ACTIVE" | grep -o "$CONFIG_DIR/[^ ]\+\.conf")
+    for i in "${!CONFIGS[@]}"; do
+        if [[ "${CONFIGS[$i]}" == "$ACTIVE_CONF" ]]; then
+	    INDEX=$i
+            break
+        fi
+    done
+    pkill -f "conky -c $ACTIVE_CONF"
+else
+    INDEX=-1
+fi
+
+# Handle action
+case "$ACTION" in
+    next)
+        NEXT_INDEX=$(( (INDEX + 1) % ${#CONFIGS[@]} ))
+        conky -c "${CONFIGS[$NEXT_INDEX]}" -D &
+        ;;
+    prev)
+        PREV_INDEX=$(( (INDEX - 1 + ${#CONFIGS[@]}) % ${#CONFIGS[@]} ))
+        conky -c "${CONFIGS[$PREV_INDEX]}" -D &
+        ;;
+    "")
+        conky -c "${CONFIGS[0]}" -D &
+        ;;
+    *)
+        notify-send "mode_conky: Conky failed to start."
+        ;;
+esac
+
+# Raise the window
+xdotool search --sync --classname "conky" windowraise
