5b7f87259d6242573af1e0b1aca93d0a085fdbc5
Nicholas <mercy.recommends@gmail.com>
2025-04-20 07:56:25 +0100

    Avoid empty str giving false +ve

--- .check-updates.sh
+++ .check-updates.sh
@@ -3,9 +3,9 @@
 # Get available updates - checkupdates is part of pacman-contrib
 updates=$(checkupdates)
 
-# Count lines
-count=$(echo "$updates" | wc -l)
-
-if (( count > 0 )); then
+# Check if updates is empty
+if [[ -n "$updates" ]]; then
+    # Count lines
+    count=$(echo "$updates" | wc -l)
     notify-send "$count package(s) can be updated" "$(echo "$updates" | head -n 10)"
 fi
