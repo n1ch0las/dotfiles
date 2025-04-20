821a753cb80394a3ed48ce168b20a936b30c9cb1
Nicholas <mercy.recommends@gmail.com>
2025-04-20 17:02:54 +0100

    Handle aborted scrot, plus Picom changes

--- .screenshot/scrot.sh
+++ .screenshot/scrot.sh
@@ -10,39 +10,51 @@ display="eDP-1"
 # Argument passed to the script
 mode=$1
 
+# Scrot status
+success=false
+
 # Take screenshot based on the mode argument
 case $mode in
   "window")
-    scrot --focused --border "$file"
+    if scrot --focused --border "$file"; then
+        success=true
+    fi
     ;;
   "selection")
     # Kill picom if running as it interferes with selection
-    if pgrep -x "picom" > /dev/null; then
-        killall picom
-        picom_was_running=true
-    else
-        picom_was_running=false
-    fi
+    #if pgrep -x "picom" > /dev/null; then
+     #   killall picom
+     #   picom_was_running=true
+    #else
+     #   picom_was_running=false
+    #fi
 
     # Screenshot of selected region
-    scrot --select "$file"
+    if scrot --select "$file"; then
+        success=true
+    fi
     
     # Restart picom if necessary
-    if [ "$picom_was_running" = true ]; then
-        picom &
-    fi
+    #if [ "$picom_was_running" = true ]; then
+    #    picom &
+    #fi
     ;;
   *)
     # default to fullscreen shot
-    scrot "$file"
+    if scrot "$file"; then
+        success=true
+    fi
     ;;
 esac
 
-# Flash
-xrandr --output "$display" --brightness 0.1
-sleep 0.1
-xrandr --output "$display" --brightness 1.0
+# Only do flash and shutter sound if screenshot succeeded
+if [ "$success" = true ]; then
+    # Flash
+    xrandr --output "$display" --brightness 0.1
+    sleep 0.1
+    xrandr --output "$display" --brightness 1.0
 
-# Shutter sound
-paplay ~/.screenshot/shutter.mp3
+    # Shutter sound
+    paplay ~/.screenshot/shutter.mp3
+fi
 
